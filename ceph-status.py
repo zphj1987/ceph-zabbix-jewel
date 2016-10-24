7f7fbdc4877e5e428b78dcea5cfe9b762ce93585
zphj1987 <zphj1987@gmail.com>
2016-10-24 23:51:29 +0800

    获取集群状态完成
    返回集群的值采用数字状态的，便于后面进行状态的处理

--- ceph-status.py
+++ ceph-status.py
@@ -11,13 +11,21 @@ def main():
         try:
             print get_cluster_health()
         except:
-            print "unknown"
+            print "255"
 
 ##get ceph cluster status
 def get_cluster_health() :
-    cluster_health = commands.getoutput('ceph health -f json-pretty 2>/dev/null')
-    json_str = json.loads(cluster_health)
-    return json_str["overall_status"]
+    cluster_health = commands.getoutput('timeout 10 ceph health -f json-pretty 2>/dev/null')
+    try:
+        json_str = json.loads(cluster_health)
+        if json_str["overall_status"] == "HEALTH_OK":
+            return 1
+        elif  json_str["overall_status"] == "HEALTH_WARN":
+            return 2
+        elif  json_str["overall_status"] == "HEALTH_ERR":
+            return 3
+    except:
+        return 255
 
 if __name__ == '__main__':
     main()
