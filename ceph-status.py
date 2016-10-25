d4c28679cd805f74e811e5235c210b596a8f0077
zphj1987 <zphj1987@gmail.com>
2016-10-25 23:31:30 +0800

    完成集群的可用容量的获取
    完成集群的可用容量的获取
    可用容量返回三位小数

--- ceph-status.py
+++ ceph-status.py
@@ -5,13 +5,17 @@ import sys
 import commands
 import json
 
-
 def main():
     if sys.argv[1] == 'health':
         try:
             print get_cluster_health()
         except:
             print "255"
+    if sys.argv[1] == 'used_percent':
+        try:
+            print get_cluster_used_percent()
+        except:
+            print 0
 
 ##get ceph cluster status
 def get_cluster_health() :
@@ -26,6 +30,16 @@ def get_cluster_health() :
             return 3
     except:
         return 255
+##get cluster used percent
+def get_cluster_used_percent():
+    cluster_used_percent = commands.getoutput('timeout 10 ceph -s -f json-pretty 2>/dev/null')
+    try:
+        json_str = json.loads(cluster_used_percent)
+        cluster_used = int(json_str["pgmap"]["bytes_used"])
+        cluster_total = int(json_str["pgmap"]["bytes_total"])
+        return    "%.3f"   %(cluster_used/float(cluster_total))
+    except:
+        return 0
 
 if __name__ == '__main__':
     main()
