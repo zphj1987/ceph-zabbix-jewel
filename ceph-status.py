4744d1ad5beaa9e4ac942fe162715c8d9be3ef5b
zphj1987 <zphj1987@gmail.com>
2016-10-27 00:06:28 +0800

    完成读写带宽和ops相关代码
    完成读写带宽和ops相关代码
    加入判断，如果key不存在就返回0，尽量不抛异常

--- ceph-status.py
+++ ceph-status.py
@@ -36,6 +36,22 @@ def main():
             print get_cluster_latency("ave_apply")
         except:
             print 0
+    if sys.argv[1] == 'throughput_write':
+        try:
+            print get_cluster_throughput("write")
+        except:
+            print 0
+    if sys.argv[1] == 'throughput_read':
+        try:
+            print get_cluster_throughput("read")
+        except:
+            print 0
+    if sys.argv[1] == 'total_ops':
+        try:
+            print get_cluster_total_ops()
+        except:
+            print 0
+
 
 ##get ceph cluster status
 def get_cluster_health() :
@@ -102,5 +118,42 @@ def get_cluster_latency(arg):
             return sum(osd_apply_list)/len(osd_apply_list)
         except:
             return 0
+#get cluster throughput write and read
+def get_cluster_throughput(arg):
+    if arg == "write":
+        try:
+            get_cluster_throughput_write = commands.getoutput('timeout 10 ceph -s -f json-pretty 2>/dev/null ')
+            json_str = json.loads(get_cluster_throughput_write)
+            if json_str["pgmap"].has_key('write_bytes_sec') == True:
+                return  json_str["pgmap"]["write_bytes_sec"]
+            else:
+                return 0
+        except:
+            return 0
+    if arg == "read":
+        try:
+            get_cluster_throughput_read = commands.getoutput('timeout 10 ceph -s -f json-pretty 2>/dev/null ')
+            json_str = json.loads(get_cluster_throughput_read)
+            if json_str["pgmap"].has_key('read_bytes_sec') == True:
+                return json_str["pgmap"]["read_bytes_sec"]
+            else:
+                return 0
+        except:
+            return 0
+# get cluster ops (read ,write,promote)
+def get_cluster_total_ops():
+    ops_list =[]
+    try:
+        cluster_total_ops = commands.getoutput('timeout 10 ceph -s -f json-pretty 2>/dev/null')
+        json_str = json.loads(cluster_total_ops)
+        if json_str["pgmap"].has_key('write_op_per_sec') == True:
+            ops_list.append(int(json_str["pgmap"]["write_op_per_sec"]))
+        if json_str["pgmap"].has_key('read_op_per_sec') == True:
+            ops_list.append(int(json_str["pgmap"]["read_op_per_sec"]))
+        if json_str["pgmap"].has_key('promote_op_per_sec') == True:
+            ops_list.append(int(json_str["pgmap"]["promote_op_per_sec"]))
+        return sum(ops_list)
+    except:
+        return 0
 if __name__ == '__main__':
     main()
